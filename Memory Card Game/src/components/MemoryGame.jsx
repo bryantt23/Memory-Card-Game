8651a00324d6692710d0fd228357016a52935f67
Britteany <miszbritteany@gmail.com>
2024-02-23 23:25:23 -0500

    created flipping conditions

--- Memory Card Game/src/components/MemoryGame.jsx
+++ Memory Card Game/src/components/MemoryGame.jsx
@@ -1,19 +1,35 @@
 import React from "react";
+import { useState } from "react";
 import "../styles/MemoryGame.css";
 import shuffle from "../shuffle";
 
 const items = [1, 2, 3, 4, 5];
 const allItems = shuffle([...items, ...items]);
+const defaultState = { index: null, value: null };
 
 const handleClick = () => {};
 
 export default function MemoryGame() {
+  const [firstCard, setFirstCard] = useState(defaultState);
+  const [secondCard, setSecondCard] = useState(defaultState);
+  const [remainingCards, setRemainingCards] = useState(items);
+  const [moves, setMoves] = useState(0);
+
   return (
     <>
       <div className="cardsContainer">
         {allItems.map((item, index) => {
           return (
-            <div key={index} className="card" onClick={handleClick}>
+            <div
+              key={index}
+              className={`card ${
+                (firstCard.index === index ||
+                  secondCard.index === index ||
+                  !remainingCards.includes(item)) &&
+                "flipped"
+              }`}
+              onClick={handleClick}
+            >
               <div className="backSide"></div>
               {item}
             </div>
