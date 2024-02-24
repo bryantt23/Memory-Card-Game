ceee76eb27c370a78a7b77802dd3ce8ae0cd6837
Britteany <miszbritteany@gmail.com>
2024-02-23 23:02:54 -0500

    added styling for game cards

--- Memory Card Game/src/App.jsx
+++ Memory Card Game/src/App.jsx
@@ -1,13 +1,12 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
+import MemoryGame from "./components/MemoryGame";
 
 function App() {
-
   return (
-    <>
-      
-            </>
-  )
+    <div className="App">
+      <MemoryGame />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- Memory Card Game/src/components/MemoryGame.jsx
+++ Memory Card Game/src/components/MemoryGame.jsx
@@ -3,7 +3,7 @@ import "../styles/MemoryGame.css";
 import shuffle from "../shuffle";
 
 const items = [1, 2, 3, 4, 5];
-const allItems = shuffle({ ...items, ...items });
+const allItems = shuffle([...items, ...items]);
 
 const handleClick = () => {};
 
@@ -12,7 +12,12 @@ export default function MemoryGame() {
     <>
       <div className="cardsContainer">
         {allItems.map((item, index) => {
-          return <div key={index} className="card" onClick={handleClick}></div>;
+          return (
+            <div key={index} className="card" onClick={handleClick}>
+              <div className="backSide"></div>
+              {item}
+            </div>
+          );
         })}
       </div>
     </>
