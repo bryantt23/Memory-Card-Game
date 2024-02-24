924dcd953129b87576f34e1566e5957ce8703de6
Britteany <miszbritteany@gmail.com>
2024-02-23 21:56:49 -0500

    added some styling, allow for card shuffle

---
+++ Memory Card Game/src/components/MemoryGame.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "../styles/MemoryGame.css";
+import shuffle from "../shuffle";
+
+const items = [1, 2, 3, 4, 5];
+const allItems = shuffle({ ...items, ...items });
+
+const handleClick = () => {};
+
+export default function MemoryGame() {
+  return (
+    <>
+      <div className="cardsContainer">
+        {allItems.map((item, index) => {
+          return <div key={index} className="card" onClick={handleClick}></div>;
+        })}
+      </div>
+    </>
+  );
+}
