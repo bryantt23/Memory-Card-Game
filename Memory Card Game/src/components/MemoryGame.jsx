ff6a7c0cf0f233a07a8086412b840f3fb92c427d
Britteany <miszbritteany@gmail.com>
2024-02-24 00:38:41 -0500

    Added images using an API

--- Memory Card Game/src/components/MemoryGame.jsx
+++ Memory Card Game/src/components/MemoryGame.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import "../styles/MemoryGame.css";
 import shuffle from "../shuffle";
 
@@ -7,16 +7,43 @@ const items = [1, 2, 3, 4, 5];
 const allItems = shuffle([...items, ...items]);
 const defaultState = { index: null, value: null };
 
-const handleClick = () => {};
-
 export default function MemoryGame() {
   const [firstCard, setFirstCard] = useState(defaultState);
   const [secondCard, setSecondCard] = useState(defaultState);
   const [remainingCards, setRemainingCards] = useState(items);
   const [moves, setMoves] = useState(0);
 
+  const timer = useRef();
+
+  const handleClick = (index, value) => {
+    clearTimeout(timer.current);
+
+    timer.current = setTimeout(() => {
+      setFirstCard(defaultState);
+      setSecondCard(defaultState);
+    }, 1000);
+
+    if (
+      firstCard.index === null ||
+      (firstCard.index !== null && secondCard.index !== null)
+    ) {
+      setSecondCard(defaultState);
+      setFirstCard({ index, value });
+      setMoves((moves) => moves + 1);
+    } else if (secondCard.index === null && firstCard.index !== index) {
+      setSecondCard({ index, value });
+      setMoves((moves) => moves + 1);
+
+      if (firstCard.value === value) {
+        setRemainingCards(remainingCards.filter((card) => card !== value));
+      }
+    }
+  };
+
   return (
     <>
+      {remainingCards.length > 0 ? `Remaining cards: ` : "Victory!"}
+      {remainingCards.map((card, index) => card)}
       <div className="cardsContainer">
         {allItems.map((item, index) => {
           return (
@@ -28,14 +55,15 @@ export default function MemoryGame() {
                   !remainingCards.includes(item)) &&
                 "flipped"
               }`}
-              onClick={handleClick}
+              onClick={() => handleClick(index, item)}
             >
               <div className="backSide"></div>
-              {item}
+              <img alt={`cat ${index}`} src={`https://robohash.org/${item}?set=set4&&size=120x120`}/>
             </div>
           );
         })}
       </div>
+      Moves used: {moves}
     </>
   );
 }
